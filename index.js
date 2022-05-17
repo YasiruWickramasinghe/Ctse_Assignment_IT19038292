1dddc60d1ed23b84cb7159a2dcafaa6827866efa
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-05-17 10:31:54 +0530

    sed updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => {
-  res.send('Authentication Backend Microsrvice Running')
+  res.send('Authentication Microsrvice Running')
 })
 
 const port = process.env.PORT || 3000
